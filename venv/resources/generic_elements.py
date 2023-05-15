1a5844c6668e3fc7f28062818babeb6c6a522390
deezyuno <qaqctest@yahoo.com>
2023-05-15 09:50:44 -0400

    update

--- venv/resources/generic_elements.py
+++ venv/resources/generic_elements.py
@@ -8,7 +8,11 @@ import random
 firname = ["Sansa", "Sirjames", "Audi", "Tokyo", "Ahmiracle", "Elizabreth", "Aliviyah", "Beberly", "Merica", "Tu Morrow", "Meldor", "Hashtag", "Jermajesty"]
 laname = ["Lopez", "Whitlock", "Kingsbury", "Jackson", "Riggins", "Macklin", "Holbrook", "Hearst", "Harrington", "Rockefeller", "Vanderbilt", "Berkshire"]
 
-search = "//textarea[@aria-label='Search']"
+gsearch = "//textarea[@aria-label='Search']"
+rdemo = "(//button[text()='Request a Demo'])[1]"
+schoollist = "//a[contains(text(),'High Schools, Clubs & Colleges')]"
+
+
 
 email = "test@test.com"
 phone = "555-555-5555"
