484eecf3c9110b7f3f2fbae3f4c80466083fc533
deezyuno <qaqctest@yahoo.com>
2023-05-14 20:16:21 -0400

    update

---
+++ venv/resources/generic_elements.py
@@ -0,0 +1,16 @@
+import time
+import string
+import random
+import sys
+import random
+
+# names
+firname = ["Sansa", "Sirjames", "Audi", "Tokyo", "Ahmiracle", "Elizabreth", "Aliviyah", "Beberly", "Merica", "Tu Morrow", "Meldor", "Hashtag", "Jermajesty"]
+laname = ["Lopez", "Whitlock", "Kingsbury", "Jackson", "Riggins", "Macklin", "Holbrook", "Hearst", "Harrington", "Rockefeller", "Vanderbilt", "Berkshire"]
+
+search = "//textarea[@aria-label='Search']"
+
+email = "test@test.com"
+phone = "555-555-5555"
+comp = "Acme Company"
+help = "Test is over. Will not submit in Prod..."
